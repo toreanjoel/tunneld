cac5e5b5c03e45e2725fc6e9dfa320c9fc5f21c5
Torean Joel <toreanjoel@gmail.com>
2024-11-10 16:56:35 +0200

    chore: fix responsive for navigation and content section along with content section scrolling

--- lib/sentinel_web/components/navigation.ex
+++ lib/sentinel_web/components/navigation.ex
@@ -1,7 +1,7 @@
 defmodule SentinelWeb.Components.Navigation do
   @moduledoc """
   The basic navigation component that will be wrapped around pages.
-  This will be a basic stateless component
+  This will be a basic stateless component.
   """
   use Phoenix.Component
   import SentinelWeb.CoreComponents
@@ -10,17 +10,16 @@ defmodule SentinelWeb.Components.Navigation do
   slot :inner_block, required: true
 
   @doc """
-  Show the navigation
+  Show the navigation.
   """
   def show(assigns) do
     ~H"""
-    <!-- NOTE: make a component that has the navigation -->
-    <div id={@id} class="flex flex-row lg:flex-row min-h-screen sm:height-screen">
-      <!-- Navigation -->
-      <div class="min-w-[50px] flex flex-col items-center py-3 border-r border-zinc-300">
+    <div id={@id} class="flex flex-row min-h-screen">
+      <!-- Fixed Navigation -->
+      <div class="fixed top-0 left-0 h-screen w-[50px] flex flex-col items-center py-3 border-r border-zinc-300 bg-gray-100">
         <!-- Navigation Icons with Hover Effect -->
         <div class="flex flex-col text-black">
-          <div class="cursor-pointer hover:bg-white hover:rounded-lg p-2 transition-all duration-500 transition-all duration-500">
+          <div class="cursor-pointer hover:bg-white hover:rounded-lg p-2 transition-all duration-500">
             <.icon name="hero-home" class="h-5 w-5" />
           </div>
           <div class="cursor-pointer hover:bg-white hover:rounded-lg p-2 transition-all duration-500">
@@ -37,20 +36,21 @@ defmodule SentinelWeb.Components.Navigation do
           <div class="cursor-pointer hover:bg-white hover:rounded-lg p-2 transition-all duration-500">
             <.icon name="hero-cog" class="h-5 w-5" />
           </div>
-
-          <%!-- Logout --%>
+          <!-- Logout -->
           <div phx-click="logout" class="cursor-pointer hover:bg-white hover:rounded-lg p-2 transition-all duration-500">
             <.icon name="hero-arrow-right-end-on-rectangle" class="h-5 w-5" />
           </div>
         </div>
-
         <span class="text-[10px] text-gray-500 pt-3">
           v: <%= Application.get_env(:sentinel, :version) %>
         </span>
       </div>
-      <!-- Main section -->
-      <div class="flex flex-col grow items-center justify-center w-full lg:w-3/5 p-8 max-w-[767px] mx-auto">
-        <%= render_slot(@inner_block) %>
+
+      <!-- Centered, scrollable main section -->
+      <div class="flex grow items-start md:items-center justify-center ml-[50px] overflow-y-auto">
+        <div class="flex flex-col items-center w-full lg:w-3/5 max-w-[767px] p-8">
+          <%= render_slot(@inner_block) %>
+        </div>
       </div>
     </div>
     """
