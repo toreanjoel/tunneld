7dac9de5b91b38d4f5659634f644c2db0ffadac5
Torean Joel <toreanjoel@gmail.com>
2024-11-16 03:55:22 +0200

    chore: update the services list key types

--- lib/sentinel/servers/services.ex
+++ lib/sentinel/servers/services.ex
@@ -5,7 +5,7 @@ defmodule Sentinel.Servers.Services do
   use GenServer
   require Logger
 
-  @services ["dnsmasq", "dhcpcd", "hostapd"]
+  @services [:dnsmasq, :dhcpcd, :hostapd]
   @interval 30_000
   @topic "sentinel:services"
 
