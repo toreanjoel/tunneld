22a5e7901f16caa614106610e6e8a81ad3a26c25
Torean Joel <toreanjoel@gmail.com>
2024-12-02 16:50:30 +0200

    chore: quick fix for the dev routes checks to know we are developing or not

--- lib/sentinel/servers/logs.ex
+++ lib/sentinel/servers/logs.ex
@@ -27,7 +27,7 @@ defmodule Sentinel.Servers.Logs do
   end
 
   def handle_call({:get_logs, ip}, _from, state) do
-    logs = if Mix.env() == :dev do
+    logs = if Application.get_env(:sentinel, :dev_routes) do
       Sentinel.Servers.FakeData.Logs.get_data()
     else
       filter_queries_by_ip(ip)
