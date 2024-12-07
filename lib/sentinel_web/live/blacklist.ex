06662076b45b199c98e85dfc05de36f3dbbe01f1
Torean Joel <toreanjoel@gmail.com>
2024-12-07 02:57:11 +0200

    chore: add blacklist table and header with view updates

--- lib/sentinel_web/live/blacklist.ex
+++ lib/sentinel_web/live/blacklist.ex
@@ -38,20 +38,44 @@ defmodule SentinelWeb.Live.Blacklist do
   """
   def render(assigns) do
     ~H"""
-    <Navigation.show id="nav">
+    <Navigation.show id="nav" align="start">
       <div class="text-left">
-        <h2>Blacklist</h2>
-        <p :if={@count == 0}>No domains blocked</p>
+      <div class="text-3xl md:text-5xl py-2 font-bold bg-gradient-to-r from-gray-700 to-gray-300 bg-clip-text text-transparent">
+          Blacklist
+        </div>
+        <%!-- This will be the basic text information that could be informational but some insights --%>
+        <div class="py-1 text-sm text-gray-600 flex flex-row items-center">
+          <div class="grow">
+            List of domains that are blocked from being accessed
+          </div>
+          <div phx-click="refresh" class="cursor-pointer hover:bg-white p-1 hover:rounded-lg transition-all duration-500">
+            <.icon name="hero-plus-circle" class="h-5 w-5" />
+          </div>
+        </div>
 
-        <div :if={@count > 0}>
-          <%= for domain <- @blacklist do %>
-            <div><%= domain %></div>
-          <% end %>
+        <hr class="my-3 border-dashed border-gray-300" />
+
+        <p :if={@count == 0}>No domains blocked</p>
+        <div :if={@count > 0} class="overflow-x-auto">
+          <table class="table-auto border-collapse border border-gray-200 w-full">
+            <thead>
+              <tr class="bg-gray-100">
+                <th class="border border-gray-300 px-4 py-2 text-left">Domain</th>
+              </tr>
+            </thead>
+            <tbody>
+              <%= for domain <- @blacklist do %>
+                <tr class="hover:bg-gray-50 cursor-pointer">
+                  <td class="border border-gray-300 px-4 py-2"><%= domain %></td>
+                </tr>
+              <% end %>
+            </tbody>
+          </table>
         </div>
 
         <div class="py-2 flex flex-row justify-between">
           <button
-            :if={@curr_page > 0}
+            disabled={@curr_page == 0}
             phx-click="prev_page"
             class="bg-zinc-900 hover:bg-zinc-700 text-white font-bold py-2 px-4 rounded"
           >
