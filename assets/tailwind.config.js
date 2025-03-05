eeb163224763f602fed555cc880c2e0b02c879de
Torean Joel <toreanjoel@gmail.com>
2025-03-05 16:35:58 +0200

    chore: custom header colors for the sidebar testing (wip) - adding new light purple

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         red: "#de5555",
         yellow: "#f2c353",
         purple: "#885fee",
-        light_purple: "#9294c0",
+        light_purple: "#656679",
         gray: {
           "1": "#a6a8b3",
           "2": "#6b6b6d"
