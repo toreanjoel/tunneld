9b1d47e0dd538cc8b798f233f1782e7a228980e4
Torean Joel <toreanjoel@gmail.com>
2025-03-07 17:29:14 +0200

    chore: add important comments for wlan interactions with the OS

--- lib/sentinel/servers/wlan.ex
+++ lib/sentinel/servers/wlan.ex
@@ -16,23 +16,39 @@ defmodule Sentinel.Servers.Wlan do
     {:ok, %{}}
   end
 
+  #  echo 'ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+  # update_config=1
+  # country=ZA
+
+  # network={
+  #     ssid="YourSSID"
+  #     psk="YourPassword"
+  #     key_mgmt=WPA-PSK
+  #     auth_alg=OPEN
+  # }' | tee /etc/wpa_supplicant/wpa_supplicant-wlan1.conf > /dev/null
+
+  # ------ important functions ----
+
+  # check if the device is connected to a router
+  # iw dev wlan1 link
+
   # getting the list wlan devices - this can be used to know what device you want to setup as the ap vs client
   # iw dev
 
   # Scanning for SSIDs
-  # iw dev wlan0 scan | grep SSID
+  # iw dev wlan1 scan | grep SSID
 
   # add details for the SSID of choice
   # config needs to be made that will be writted to
-  # wpa_passphrase "YourSSID" "YourPassword" > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+  # wpa_passphrase "YourSSID" "YourPassword" > /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
 
   # Restart the wpa_supplicant service after writing to it with the details (starting)
-  # wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-
+  # wpa_supplicant -B -i wlan1 -c /etc/wpa_supplicant/wpa_supplicant-wlan1.conf
 
   # Stopping the connection to the wifi connection
-  # killall wpa_supplicant
+  # pkill wpa_supplicant
 
+  # -------------------------------
 
   # Config helper - we need to set the wlan configuration on the env variables and runtime config?
   # defp config_fs(), do: Application.get_env(:sentinel, :fs)
