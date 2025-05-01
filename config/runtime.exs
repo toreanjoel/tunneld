34ce888f12b1abc32221f1d5e176c156a08aa235
Torean Joel <toreanjoel@gmail.com>
2025-05-01 20:24:42 +0200

    chore: update the cert expectation

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,7 +53,7 @@ if config_env() == :prod do
   config :sentinel, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :sentinel, SentinelWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 80, scheme: "http"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
