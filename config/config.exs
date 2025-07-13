ee78e6efec5329e5f220890ec0744861fdba450c
Torean Joel <toreanjoel@gmail.com>
2025-07-13 22:42:55 +0200

    chore: version bump

--- config/config.exs
+++ config/config.exs
@@ -79,7 +79,7 @@ config :tunneld, :network,
   mullvad: "wg0-mullvad",
   gateway: "10.0.0.1"
 
-config :tunneld, version: "0.2.3"
+config :tunneld, version: "0.2.4"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
