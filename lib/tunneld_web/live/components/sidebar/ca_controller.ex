9bd722267dbbb1072f16c71da87c519c8b5fde04
Torean Joel <toreanjoel@gmail.com>
2026-01-18 03:43:13 +0200

    chore: downloader of the RootCA file throug the controller

--- lib/tunneld_web/live/components/sidebar/ca_controller.ex
+++ lib/tunneld_web/live/components/sidebar/ca_controller.ex
@@ -2,15 +2,24 @@ defmodule TunneldWeb.CAController do
   use TunneldWeb, :controller
 
   def download(conn, _params) do
-    certs_config = Application.get_env(:tunneld, :certs)
-    ca_path = Path.join(certs_config[:ca_dir], certs_config[:ca_file])
+    certs_config = Application.get_env(:tunneld, :certs) || []
+    ca_dir = certs_config[:ca_dir]
+    ca_file = certs_config[:ca_file]
 
-    conn
-    |> put_resp_content_type("application/x-x509-ca-cert")
-    |> put_resp_header(
-      "content-disposition",
-      "attachment; filename=\"#{certs_config[:ca_file]}\""
-    )
-    |> send_file(200, ca_path)
+    if ca_dir && ca_file do
+      # Ensure the path is absolute and correctly resolved
+      ca_path = Path.expand(Path.join(ca_dir, ca_file))
+
+      if File.exists?(ca_path) do
+        conn
+        |> put_resp_content_type("application/x-x509-ca-cert")
+        |> put_resp_header("content-disposition", "attachment; filename=\"#{ca_file}\"")
+        |> send_file(200, ca_path)
+      else
+        conn |> put_status(404) |> text("Certificate file not found.")
+      end
+    else
+      conn |> put_status(500) |> text("CA configuration is missing or incomplete.")
+    end
   end
 end
