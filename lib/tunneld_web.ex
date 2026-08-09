e0c50e79ce2598e06b28062dc1ecf4bf62ee310d
Torean Joel <toreanjoel@gmail.com>
2026-08-09 15:08:04 +0200

    fix(ui): serve fonts via Plug.Static (add fonts to static_paths)

--- lib/tunneld_web.ex
+++ lib/tunneld_web.ex
@@ -17,7 +17,7 @@ defmodule TunneldWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets images fonts favicon.ico robots.txt)
 
   def router do
     quote do
