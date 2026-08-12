cdc038af1a78648ee09892b919e8382ed715ea88
Torean Joel <toreanjoel@gmail.com>
2026-08-12 14:15:48 +0200

    chore: bump to 0.19.9

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tunneld.MixProject do
   def project do
     [
       app: :tunneld,
-      version: "0.19.8",
+      version: "0.19.9",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
